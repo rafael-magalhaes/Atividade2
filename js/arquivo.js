04ac99fe958b8c1306850f5d98ba32110e24d7a8
Rafael Magalhães <rmfbm@outlook.com>
2018-08-04 10:24:32 -0300

    Adicionando carousel no template

---
+++ js/arquivo.js
@@ -0,0 +1,7 @@
+function enviar(){var nome = document.getElementById('txt-nome');
+if (nome.value != "") {
+  alert("Obrigado, "+nome.value+". Esta área ainda não está dispnível.");
+} else {
+    alert("Obrigado, mas esta área ainda não está disponível.");
+  }
+}
